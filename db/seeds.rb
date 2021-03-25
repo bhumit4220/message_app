2aa6f03660d70e9b8f2fb23e5a61e178db236b75
bhumit4220 <bhumit2520@gmail.com>
2021-03-25 11:30:10 +0530

    complete messaging part without websocket

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Message.create(body: "hii, i'm message - 1", user: User.first)
+Message.create(body: "hii, i'm message - 2", user: User.second)
+Message.create(body: "hii, i'm message - 3", user: User.third)
+Message.create(body: "hii, i'm message - 4", user: User.fourth)
+Message.create(body: "hii, i'm message - 5", user: User.fifth)
