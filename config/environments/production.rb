4ed32d7d48c7e5447518ccd10e9b68a2d27ea076
bhumit4220 <bhumit2520@gmail.com>
2021-03-25 14:30:46 +0530

    Add route to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_cable.allowed_request_origins = ['https://secure-cove-53768.herokuapp.com/', 'http://secure-cove-53768.herokuapp.com/']
 end
